ae5f67ca2ed422f8fc8143f0f851b97f398d5fb6
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-02-07 22:02:27 +0600

    update_homeportfolio_0001

--- src/Pages/Home/Homesection/Homeportfolio/Homeportfoio.jsx
+++ src/Pages/Home/Homesection/Homeportfolio/Homeportfoio.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Homeportfolio.css'
-import Subhead from '../../../../Utilities/Subhead/subhead'
 import portfolioone from '../../../../images/portfolioone.jpg'
 import { Link } from 'react-router-dom'
+import Subhead from '../../../../Utilities/Subhead/Subhead'
 
 const Homeportfoio = () => {
   return (
